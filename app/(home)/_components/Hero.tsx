0a48f7b3685f67c2ac8333da04269e9f79c9a040
Philani Ncube <ncbphi001@gmail.com>
2023-10-02 19:50:34 +0200

    hero image

--- app/(home)/_components/Hero.tsx
+++ app/(home)/_components/Hero.tsx
@@ -6,24 +6,26 @@ const Hero = () => {
     <div className="relative overflow-clip">
       <div className="absolute inset-0">
         <Image
-          src="/images/solar_home.webp"
+          src="/images/living-room.webp"
           width={1920}
           height={1080}
           alt="Solar"
-          className="w-full aspect-video object-cover rounded-md"
-        /></div>
-      <section className="container relative z-20 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-10 py-8">
-        <div className="flex flex-col justify-center w-full  py-20 ">
+          className="object-cover w-full rounded-md aspect-video"
+        />
+      </div>
+      <section className="container relative z-20 grid grid-cols-1 gap-6 py-8 mx-auto lg:grid-cols-2 md:gap-10">
+        <div />
+        <div className="flex flex-col justify-center w-full py-20 ">
           <h1 className="text-3xl font-bold uppercase md:text-5xl max-w-[20ch] text-zinc-800">
             Solar INstallations for Off-Grid & Load Shedding
           </h1>
-          <p className="mt-5 text-md text-zinc-600 font-medium pr-20">
+          <p className="pr-20 mt-5 font-medium text-md text-zinc-600">
             Unlocking Renewable Energy Solutions: High-quality, Affordable Solar
             Installations for a Sustainable Future
           </p>
           <Button
             type="button"
-            className="mt-5 text-white w-fit uppercase rounded-full text-md bg-red-600"
+            className="mt-5 text-white uppercase bg-red-600 rounded-full w-fit text-md"
           >
             Get A Quote
           </Button>
@@ -33,7 +35,7 @@ const Hero = () => {
         width={1456}
         height={1224}
         alt="Solar"
-        className="w-full aspect-video object-cover rounded-md"
+        className="object-cover w-full rounded-md aspect-video"
       /> */}
       </section>
     </div>
