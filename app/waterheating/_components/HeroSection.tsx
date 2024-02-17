5fbb7cc42b04c30a53b9e797d70c98f7c225d22b
Philani Ncube <ncbphi001@gmail.com>
2024-02-17 17:46:25 +0200

    Update image file extensions and adjust CSS classes

--- app/waterheating/_components/HeroSection.tsx
+++ app/waterheating/_components/HeroSection.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const HeroSection = () => {
     return (
-      <section className="w-full relative">
+      <section className="relative w-full">
         <Image
-          src="/images/solar-geyser.jpg"
+          src="/images/solar-geyser.webp"
           width={1920}
           height={1280}
           alt="geyser"
           className="w-full object-cover max-h-[60vh] min-h-[50vh] object-bottom"
         />
         <div className="absolute inset-0 bg-gradient-to-r from-zinc-700 to-slate-500/80 lg:to-slate-500/40">
-          <div className="container mx-auto flex flex-col justify-center items-start h-full">
+          <div className="container flex flex-col items-start justify-center h-full mx-auto">
             <h1 className="text-2xl lg:text-5xl font-bold text-white max-w-[20ch]">
               Solar Geysers/Water Heating
             </h1>
@@ -21,7 +21,7 @@ const HeroSection = () => {
               Embrace the power of the sun and save on your water heating costs
               with our top-quality solar water heaters/geysers.
             </p>
-            <Button className="bg-red-600 text-white rounded-full mt-6">
+            <Button className="mt-6 text-white bg-red-600 rounded-full">
               Request Quote
             </Button>
           </div>
